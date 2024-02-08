95d75f6260ad176d6360681f04728986531d78cd
= <info@josevicentecarratala.com>
2024-02-08 18:35:36 +0100

    Servicio para comunicar en red

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatosService } from './datos.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemplomean';
+  jsonData: any;
+
+  constructor(private datosService: DatosService) { }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.datosService.getData().subscribe(data => {
+      this.jsonData = data;
+    });
+  }
 }
